e0de513dbe8db2564be5e58dc6ca38eb559ff5a1
Martin Cardozo <martin.cardozo.25@outlook.com.ar>
2023-05-15 15:48:01 -0300

    changing slide tracks visibility

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -29,7 +29,7 @@ function Gallery() {
     <div className="slider">
 
         {/* imagenes 1 a 4 */}
-        <div className="slide-track-up">
+        <div className="slide-track-up" id='slide-track-1'>
             <div className="slide">
                 <img src={images[0]} alt="slides"/>
             </div>
@@ -58,7 +58,7 @@ function Gallery() {
         </div>
 
         {/* imagenes 5 a 8 */}
-        <div className="slide-track-down">
+        <div className="slide-track-down" id='slide-track-2'>
             <div className="slide">
                 <img src={images[4]} alt="slides"/>
             </div>
@@ -87,7 +87,7 @@ function Gallery() {
         </div>
 
         {/* imagenes 9 a 12 */}
-        <div className="slide-track-up">
+        <div className="slide-track-up" id='slide-track-3'>
             <div className="slide">
                 <img src={images[8]} alt="slides"/>
             </div>
@@ -116,7 +116,7 @@ function Gallery() {
         </div>
 
         {/* imagenes 13 a 16 */}
-        <div className="slide-track-down">
+        <div className="slide-track-down" id='slide-track-4'>
             <div className="slide">
                 <img src={images[12]} alt="slides"/>
             </div>
